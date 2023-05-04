9061ef3f3fb0aa807606de0c3b9f9ed7db6def9d
GFilippos <gomelisfil@gmail.com>
2023-05-04 20:20:25 +0300

    push to remote repo

---
+++ app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur optio eaque adipisci natus deserunt nostrum
+        dolorem laudantium amet voluptatem? Iste eum architecto sint ex voluptatibus, expedita reiciendis modi nisi
+        doloribus.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,13 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto quis soluta obcaecati, quas nulla inventore
+        quaerat repellendus dolor exercitationem. Quod harum ipsum vitae? Nemo iste saepe quos quas, error veniam.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
